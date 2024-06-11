1ec7390efbbfd4d2deb16776d6bc7e8a62e36454
Hunterlios <jakubx1001@gmail.com>
2024-06-11 22:39:14 +0200

    repository pull

--- src/main/java/com/example/platform/auth/AuthenticationController.java
+++ src/main/java/com/example/platform/auth/AuthenticationController.java
@@ -1,12 +1,10 @@
 package com.example.platform.auth;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/auth")
 public class AuthenticationController {
 
