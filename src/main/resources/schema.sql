293fab1696c3cf24e546709c0636558821eaca52
Natalia Staszak <natalia.staszak@sap.com>
2024-09-26 19:12:23 +0200

    first commit

--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS user(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    firstName VARCHAR(255),
-    lastName VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(255),
-    role VARCHAR(255)
-    );
